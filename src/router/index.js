0720a162ddd6233c2923cbd0d6c4910b798165c5
Thanh Dat <thanhdatphan725@gmail.com>
2024-03-27 10:06:13 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+        },
  ];
  const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
